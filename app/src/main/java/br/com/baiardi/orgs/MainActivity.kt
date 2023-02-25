2837f7f8896727856b85076383a8cffe3ebb1ada
Breno Baiardi <brenocbo@gmail.com>
2023-02-25 13:20:56 -0300

    add: primeiro comportamento no dispositivo

--- app/src/main/java/br/com/baiardi/orgs/MainActivity.kt
+++ app/src/main/java/br/com/baiardi/orgs/MainActivity.kt
@@ -1,4 +1,14 @@
 package br.com.baiardi.orgs
 
-class MainActivity {
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(this, "Bem vindo", Toast.LENGTH_SHORT).show()
+    }
+
 }

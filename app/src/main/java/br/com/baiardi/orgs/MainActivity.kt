2da1e12ee7bb65aa79a8457636201f5b449c6d37
Breno Baiardi <brenocbo@gmail.com>
2023-02-25 14:09:23 -0300

    add: primeiros elementos na view

--- app/src/main/java/br/com/baiardi/orgs/MainActivity.kt
+++ app/src/main/java/br/com/baiardi/orgs/MainActivity.kt
@@ -2,13 +2,16 @@ package br.com.baiardi.orgs
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Bem vindo", Toast.LENGTH_SHORT).show()
+        val textView = TextView(this)
+        setContentView(R.layout.activity_main)
     }
 
 }

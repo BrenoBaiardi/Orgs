d3f777d934c32555c6c1652d8e1a96e045700d8c
Breno Baiardi <brenocbo@gmail.com>
2023-03-22 21:15:23 -0300

    add: recyclerView completamente configurado
    com adapter e com linearLayoutManager

--- app/src/main/java/br/com/baiardi/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/baiardi/orgs/ui/activity/MainActivity.kt
@@ -2,10 +2,13 @@ package br.com.baiardi.orgs.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import androidx.recyclerview.widget.RecyclerView
 import br.com.baiardi.orgs.ListaProdutosAdapter
 import br.com.baiardi.orgs.R
+import br.com.baiardi.orgs.model.Produto
+import java.math.BigDecimal
 
 class MainActivity : Activity() {
 
@@ -13,7 +16,11 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.adapter = ListaProdutosAdapter()
+        recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = listOf(
+            Produto("a","adesc", BigDecimal("19.00")),
+            Produto("b","bdesc", BigDecimal("19.00")),
+            Produto("c","cdesc", BigDecimal("19.00")),
+        ))
     }
 
 }
